2fbc742717bdf2ad45d6d3c7690133ba3391d6ae
Dimitris Touskas <37916794+DimitrisTouskas@users.noreply.github.com>
2024-03-22 15:20:02 +0200

    Update Task.jsx

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-//export const symbols = Task();
+
 export  function Task() {
   const URL ="https://api.marketstack.com/v1/tickers?access_key=01d6c2bcbee61513edbb9add41b3c1ae";
   const [symbols, setSymbols] = useState([]);
@@ -11,19 +11,19 @@ export  function Task() {
       try {
         const response = await fetch(URL);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Error');
         }
         const data = await response.json();
         const extractedSymbols = data.data.map(item => item.symbol);
         setSymbols(extractedSymbols);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error:', error);
       }
     }
     fetchData();
   }, []);
 
-  //console.log(symbols, "symbols");
+  console.log(symbols, "symbols");
 
   return symbols;
 }
